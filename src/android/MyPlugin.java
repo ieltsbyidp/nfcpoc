0a4dad747c6f99f37c08914f0551cce31faca1e6
ieltsbyidp <139210102+ieltsbyidp@users.noreply.github.com>
2024-07-18 09:58:13 +0530

    Add files via upload

---
+++ src/android/MyPlugin.java
@@ -0,0 +1,28 @@
+package com.daon.sdk.nfc;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class MyPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        // Use your AAR library functionality here
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
